4f12f6c80cd16075a594c5b43dc9800ffaa0a108
Michael <mfrancois3k@gmail.com>
2025-02-27 21:37:09 -0500

    1.1 version

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  // root: './',
-  // base: './',
-  css: {
-    preprocessorOptions: {
-      scss: {
-      
-      }
-    }
-  },
+  base: '/',  // This ensures proper path resolution
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
